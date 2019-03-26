67b990a8d9f2d17043a2a4801d8382032bb50acf
denysvol <info.denvol@gmail.com>
2019-03-26 08:27:17 +0200

    first commit

---
+++ build/webpack.config.js
@@ -0,0 +1,239 @@
+let Optimize = false;
+
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
+const SpriteLoaderPlugin = require('svg-sprite-loader/plugin');
+const WebpackBuildNotifierPlugin = require('webpack-build-notifier');
+const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const yargs = require('yargs');
+const argv = yargs.boolean('disable-compression-plugin').argv;
+
+const StyleLintPlugin = require('stylelint-webpack-plugin');
+const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
+const ParallelUglifyPlugin = require('webpack-parallel-uglify-plugin');
+
+const consoleStats = {
+	all: false,
+	modules: true,
+	maxModules: 0,
+	errors: true,
+	warnings: true,
+	moduleTrace: true,
+	errorDetails: true
+};
+
+module.exports = (env) => {
+	let prod = env.NODE_ENV === 'production';
+	return {
+		mode: prod ? 'production' : 'development',
+		devtool: prod ? 'none' : 'inline-source-map',
+		context: path.resolve(__dirname, '../src'),
+		cache: true,
+		entry: {
+			app: './app.js'
+		},
+		output: {
+			path: path.resolve(__dirname, '../dist'),
+			publicPath: prod ? './' : '/',
+			filename: 'assets/js/[name].bundle.js'
+		},
+		devServer: {
+			contentBase: path.resolve(__dirname, '../src'),
+			overlay: true,
+			compress: true,
+			port: 9090,
+			stats: consoleStats
+		},
+		stats: consoleStats,
+		performance: {
+			hints: false
+		},
+		module: {
+			rules: [{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: [{
+					loader: 'babel-loader',
+					options: {
+						cacheDirectory: true,
+						presets: ['es2015']
+					}
+				}]
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: [{
+					loader: 'eslint-loader'
+				}]
+			},
+			{
+				test: /\.css$/,
+				use: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					use: 'css-loader'
+				})
+			},
+			{
+				test: /\.(sass|scss)$/,
+				use: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					use: [{
+						loader: 'css-loader',
+						query: {
+							importLoaders: 1,
+							sourceMap: !prod
+						}
+					},
+					'sass-loader?sourceMap'
+					]
+				})
+			},
+			{
+				test: /\.svg$/,
+				include: [
+					path.resolve(__dirname, '../src/assets/images/svg/')
+				],
+				loader: 'svg-sprite-loader',
+				options: {
+					extract: true,
+					spriteFilename: 'assets/images/svg-sprite/sprite.svg'
+				}
+			},
+			{
+				test: /\.(png|jpe?g|gif|svg|ico)(\?.*)?$/,
+				loader: 'url-loader',
+				exclude: [
+					path.resolve(__dirname, '../src/assets/images/svg/'),
+					path.resolve(__dirname, '../src/assets/fonts/'),
+					path.resolve(__dirname, '../dist/assets/svg-sptire/')
+				],
+				options: {
+					emitFile: false,
+					limit: 3000,
+					name: '../../[path][name].[ext]'
+				}
+			},
+			{
+				test: /\.(woff2?|eot|ttf|otf|svg)(\?.*)?$/,
+				include: [
+					path.resolve(__dirname, '../src/assets/fonts/')
+				],
+				loader: 'url-loader',
+				options: {
+					limit: 5000,
+					publicPath: prod ? '../../' : '/',
+					name: prod ? '[path][name].[ext]' : '../[path][name].[ext]'
+				}
+			},
+			{
+				test: /\.(mp4)(\?.*)?$/,
+				loader: 'url-loader',
+				options: {
+					limit: 10000,
+					name: 'assets/videos/[name].[hash:7].[ext]'
+				}
+			},
+			{
+				test: /\.(sass|scss)$/,
+				loader: 'import-glob-loader'
+			},
+			{
+				test: /\.pug$/,
+				loaders: ['file-loader?name=../dist/[name].html', 'pug-html-loader?pretty&exports=false']
+			}
+			]
+		},
+		plugins: (function(argv) {
+			let pluginsComplete = [
+				new SpriteLoaderPlugin({
+					plainSprite: true,
+					spriteAttrs: {
+						id: 'svg-sprite'
+					}
+				}),
+				new CopyWebpackPlugin([
+					{
+						from: '../manifest.json',
+						to: 'manifest.json'
+					},
+					{
+						from: '../browserconfig.xml',
+						to: 'browserconfig.xml'
+					},
+					{
+						from: 'assets/images',
+						to: 'assets/images'
+					}
+				]),
+				new ExtractTextPlugin({
+					filename: 'assets/css/[name].bundle.css',
+					allChunks: true
+				}),
+
+				new WebpackBuildNotifierPlugin({
+					title: 'RedSoft',
+					suppressSuccess: 'initial',
+					activateTerminalOnError: true
+				}),
+				new FriendlyErrorsWebpackPlugin({
+					compilationSuccessInfo: {
+						messages: ['You server is running here http://localhost:9090']
+					}
+				}),
+				new webpack.ProvidePlugin({
+					'window.jQuery': path.resolve(__dirname, '../node_modules/jquery/dist/jquery'),
+					'window.$': path.resolve(__dirname, '../node_modules/jquery/dist/jquery'),
+					'jQuery': path.resolve(__dirname, '../node_modules/jquery/dist/jquery'),
+					'$': path.resolve(__dirname, '../node_modules/jquery/dist/jquery'),
+					'Swiper': path.resolve(__dirname, '../node_modules/swiper/dist/js/swiper')
+				})
+			];
+
+			if (argv.env.NODE_ENV === 'production') {
+				pluginsComplete.push(
+					new OptimizeCssAssetsPlugin({
+						cssProcessor: require('cssnano'),
+						cssProcessorPluginOptions: {
+							preset: ['default', {
+								discardComments: {
+									removeAll: true
+								}
+							}]
+						},
+						canPrint: true
+					}),
+					new ImageminPlugin({
+						pngquant: {
+							quality: '70'
+						},
+						test: /\.(jpe?g|png|gif)$/i
+					})
+				);
+			}
+
+			if (Optimize) { //! работает не всегда хорошо, нужно тестировать
+				pluginsComplete.push(
+					new HardSourceWebpackPlugin({
+						environmentHash: {
+							root: process.cwd(),
+							directories: ['assets/template', 'assets/fonts', 'assets/libs', 'assets/styles', 'assets/images/temp', 'assets/scripts']
+						}
+					}),
+					new StyleLintPlugin(),
+					new ParallelUglifyPlugin({
+						cacheDir: path.join('node_modules', '.cache', 'parallel-uglify'),
+						sourceMap: !prod,
+						uglifyES: {
+						}
+					})
+				);
+			}
+			return pluginsComplete;
+		})(argv)
+	};
+};
---
+++ src/app.js
@@ -0,0 +1,66 @@
+// Load app style
+import '../src/assets/styles/_app.scss';
+
+// JS npm scripts
+import '../node_modules/jquery.nice-number/dist/jquery.nice-number';
+import '../node_modules/magnific-popup/dist/jquery.magnific-popup.min';
+import '../node_modules/jquery-nice-select/js/jquery.nice-select.min';
+import '../node_modules/overlayscrollbars/js/jquery.overlayScrollbars.min';
+import '../node_modules/inputmask/dist/jquery.inputmask.bundle';
+
+// JS assets scripts
+import imgToSvg from '../src/assets/scripts/imgToSvg';
+import toggleTabs from '../src/assets/scripts/toggleTabs';
+
+// JS template components
+import carousel from './template/components/carousel/carousel';
+import svg4everybody from 'svg4everybody';
+
+const app = {
+	load: () => {
+		app.bindEvents();
+	},
+	bindEvents: () => {
+		let readyFUNC = {
+			mfpOpt: {
+				type: 'inline',
+				fixedContentPos: false,
+				fixedBgPos: true,
+				overflowY: 'auto',
+				closeBtnInside: true,
+				preloader: false,
+				midClick: true,
+				removalDelay: 300,
+				mainClass: 'my-mfp-slide-bottom'
+			},
+			isMobile: () => { return /iPhone|iPad|iPod|Android/i.test(navigator.userAgent); },
+			isIE: () => { return navigator.userAgent.indexOf('MSIE ') > -1 || navigator.userAgent.indexOf('Trident/') > -1; }
+		};
+
+		$('input[type="number"]').niceNumber();
+		$('select').niceSelect();
+		$('input[type="tel"]').inputmask('+7 (999) 999-99-99');
+		$('.js-popup').magnificPopup(readyFUNC.mfpOpt);
+		$('.scrollbar-outer').overlayScrollbars({});
+
+		imgToSvg();
+		carousel();
+		svg4everybody();
+		toggleTabs();
+
+		if (readyFUNC.isIE()) $('body').addClass('ie');
+		if (readyFUNC.isMobile()) $('body').addClass('touch');
+
+		setTimeout(() => {
+			$('body').addClass('show-body');
+		}, 150);
+	}
+};
+
+$(document).ready(app.load);
+
+function requireAll(r) {
+	r.keys().forEach(r);
+}
+requireAll(require.context('./assets/images/svg/', true, /\.svg$/));
+requireAll(require.context('./template/pages/', true, /\.pug$/));
